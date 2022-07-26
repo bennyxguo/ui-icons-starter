8d56e1796fd9f3054b74a7dcceb05d4ac3a1a08d
Benny Guo <bennyxguo6@gmail.com>
2022-07-26 19:05:03 +1000

    Initial commit

---
+++ showcase/webpack.config.js
@@ -0,0 +1,44 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const config = {
+  entry: './showcase/main.js',
+  output: {
+    publicPath: './',
+    path: path.resolve(__dirname, '../dist/showcase')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './showcase/index.html',
+      filename: path.resolve(__dirname, '../dist/showcase/index.html'),
+      hash: true
+    })
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader'
+      }
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ]
+  }
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
